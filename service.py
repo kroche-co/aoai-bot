db9e593b67329354930ee18571dcc568289e6bc5
Alexander Yakimenko <kukaryambik@gmail.com>
2023-03-19 23:23:57 +0000

    Update Dockerfile, service.py, requirements.txt

---
+++ service.py
@@ -0,0 +1,60 @@
+import os
+import telegram
+import openai
+
+# Устанавливаем токены и ключи из переменных окружения
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+# Инициализируем телеграм-бота
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+# Инициализируем OpenAI API
+openai.api_key = OPENAI_API_KEY
+
+# Основная функция для обработки сообщений от пользователя
+def handle_message(update, context):
+    # Получаем текст сообщения от пользователя
+    message_text = update.message.text
+
+    # Отправляем запрос в OpenAI
+    response = openai.Completion.create(
+        engine="davinci-codex",
+        prompt=message_text,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    # Получаем ответ от OpenAI
+    response_text = response.choices[0].text.strip()
+
+    # Проверяем наличие метки "EXECUTE:"
+    if response_text.startswith("EXECUTE:"):
+        # Выполняем команду
+        command = response_text.replace("EXECUTE:", "").strip()
+        command_result = os.popen(command).read()
+        response_text = f"RESPONSE: {command_result}"
+
+    # Отправляем ответ пользователю
+    chat_id = update.message.chat_id
+    bot.send_message(chat_id=chat_id, text=response_text)
+
+# Основной цикл программы
+if __name__ == '__main__':
+    # Создаем объект для работы с телеграмом
+    updater = telegram.ext.Updater(token=TELEGRAM_TOKEN, use_context=True)
+
+    # Создаем обработчик сообщений
+    message_handler = telegram.ext.MessageHandler(
+        telegram.ext.Filters.text, handle_message)
+
+    # Регистрируем обработчик сообщений
+    updater.dispatcher.add_handler(message_handler)
+
+    # Запускаем бота
+    updater.start_polling()
+
+    # Запускаем цикл обработки сообщений
+    updater.idle()
