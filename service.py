afb76142f3bea071d2ae7158de495a7a2ecf2ff3
Alexander Yakimenko <kukaryambik@gmail.com>
2023-03-22 10:26:27 +0000

    Update service.py, requirements.txt

--- service.py
+++ service.py
@@ -1,68 +1,62 @@
-import time
-import json
 import os
 import logging
+import json
+import asyncio
 import openai
 from transformers import GPT2TokenizerFast
-from telegram import ext, Bot
-from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
 from cachetools import TTLCache
-
 
 # Set tokens and keys from environment variables
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 MONGO_DB_URL = os.getenv("MONGO_DB_URL")
 
-
 # Set logging level to DEBUG
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
 )
 
-
 # Initialize Telegram bot
 bot = Bot(token=TELEGRAM_TOKEN)
-
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 # Initialize OpenAI API
 openai.api_key = OPENAI_API_KEY
 
 
-# Initialize mongoDB
-client = MongoClient(MONGO_DB_URL)
-try:
-    # The ismaster command is cheap and does not require auth.
-    client.admin.command("ismaster")
-except ConnectionFailure:
-    logging.error(f"Server not available")
-
+# Initialize MongoDB
+client = AsyncIOMotorClient(MONGO_DB_URL)
+asyncio.get_event_loop().run_until_complete(client.admin.command("ismaster"))
 db = client["telegram_bot_db"]
 conversations = db["conversations"]
 
 # Create an index for chat_id if it does not already exist
-conversations.create_index("chat_id")
+async def create_chat_id_index():
+    await conversations.create_index("chat_id")
+
+asyncio.get_event_loop().run_until_complete(create_chat_id_index())
 
 
-# Create a cache with a lifetime of 1 hour
+# Create a cache with a lifetime of 20 minutes
 cache = TTLCache(maxsize=1024, ttl=1200)
 
-
 # Handler for the /start command
-def start(update, context):
+async def start(message: types.Message):
     try:
-        context.bot.send_message(
-            chat_id=update.message.chat_id, text="Hello! I'm ready to work."
-        )
-        logging.info(f"User {update.message.chat_id} started the bot")
+        await bot.send_message(chat_id=message.chat.id, text="Hello! I'm ready to work.")
+        logging.info(f"User {message.chat.id} started the bot")
     except Exception as e:
         logging.error(f"An error occurred while processing the /start command: {e}")
 
+dp.register_message_handler(start, commands=["start"])
 
 tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
 
-def truncate_msgs_to_tokens(messages, token_limit):
+async def truncate_msgs_to_tokens(messages, token_limit):
     logging.debug("Truncuate messages")
 
     tokenized_msg = tokenizer.encode(json.dumps(messages[-1]))
@@ -83,20 +77,18 @@ def truncate_msgs_to_tokens(messages, token_limit):
 
 
 # Function to process the message with OpenAI
-def process_message_with_openai(
+async def process_message_with_openai(
     msgs, token_limit=2560, response_token_limit=1536
 ):
     logging.debug(f"Trying to send messages to ChatGPT")
 
-    # msgs.insert(-2, {"role": "system", "content": ""})
-
     try:
-        truncated_msgs = truncate_msgs_to_tokens(msgs, token_limit)
+        truncated_msgs = await truncate_msgs_to_tokens(msgs, token_limit)
     except ValueError as e:
         logging.error(e)
         return f"Error: {e}"
 
-    response = openai.ChatCompletion.create(
+    response = await openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         max_tokens=response_token_limit,
         messages=truncated_msgs,
@@ -108,41 +100,33 @@ def process_message_with_openai(
 
 
 # Load messages
-def load_messages(chat_id):
-    # If chat ID is present in the cache, return messages from the cache
+async def load_messages(chat_id):
     if chat_id in cache:
         return cache[chat_id]
 
-    # Otherwise, retrieve messages from the database
-    # Sort them by the "_id" field in descending order (from newest to oldest) and limit the number to 100
-    messages = list(conversations.find({"chat_id": chat_id}).sort("_id", -1).limit(300))
+    messages = list(await conversations.find({"chat_id": chat_id}).sort("_id", -1).limit(300).to_list(None))
 
-    # Get the _id of the oldest message among the last 100
     oldest_message_id = messages[-1]["_id"] if len(messages) > 0 else None
 
     if oldest_message_id:
-        # Delete all messages that are older than the oldest message among the last 100 messages for this chat
-        conversations.delete_many({"chat_id": chat_id, "_id": {"$lt": oldest_message_id}})
+        await conversations.delete_many({"chat_id": chat_id, "_id": {"$lt": oldest_message_id}})
         logging.debug(f"Deleted old messages for chat {chat_id}")
 
-    # If messages are found, extract their content, otherwise return an empty list
     result = [message["message"] for message in messages] if len(messages) > 0 else []
 
-    # Save the result in the cache with the chat_id as the key
     cache[chat_id] = result
     return result
 
 
 # Add message into db and update cache
-def save_messages(chat_id, messages):
+async def save_messages(chat_id, messages):
     for message in messages:
-        conversations.update_one(
+        await conversations.update_one(
             {"chat_id": chat_id, "message": message},
             {"$set": {"chat_id": chat_id, "message": message, "tags": message.get("tags", None)}},
             upsert=True,
         )
 
-    # Если чат уже находится в кеше, обновите его
     if chat_id in cache:
         cached_messages = cache[chat_id]
         for message in messages:
@@ -151,18 +135,18 @@ def save_messages(chat_id, messages):
 
 
 # Main function for handling user messages
-def handle_message(update, context):
+async def handle_message(message: types.Message):
     try:
-        message_text = update.message.text
-        chat_id = update.message.chat_id
+        message_text = message.text
+        chat_id = message.chat.id
 
-        messages = load_messages(chat_id)
+        messages = await load_messages(chat_id)
         messages.append(
             {"role": "user", "content": message_text}
         )
 
         # Process the message with OpenAI
-        response = process_message_with_openai(messages)
+        response = await process_message_with_openai(messages)
         logging.debug(f"Request sent to OpenAI for processing")
 
         # Get the response from OpenAI
@@ -172,10 +156,10 @@ def handle_message(update, context):
             messages.append(
                 {"role": "assistant", "content": response_text}
             )
-            save_messages(chat_id, messages[-2:])
+            await save_messages(chat_id, messages[-2:])
 
             # Send the response to the user
-            bot.send_message(chat_id=chat_id, text=response_text)
+            await bot.send_message(chat_id=chat_id, text=response_text)
             logging.debug(f"Sent response to user {chat_id}")
         else:
             logging.debug(f"Received empty response from OpenAI")
@@ -183,40 +167,22 @@ def handle_message(update, context):
     except Exception as e:
         logging.error(f"An error occurred while processing the user message: {e}")
         try:
-            # Try to reconnect and send the message again
-            n_bot = Bot(token=TELEGRAM_TOKEN)
-            n_bot.send_message(chat_id=chat_id, text="Sorry, something went wrong while processing your request. Please try again.")
+            await bot.send_message(chat_id=chat_id, text="Sorry, something went wrong while processing your request. Please try again.")
             logging.debug(f"Sent error message to user {chat_id}")
         except Exception as e:
             logging.error(f"An error occurred while trying to reconnect to Telegram bot: {e}")
 
+dp.register_message_handler(handle_message, content_types=types.ContentTypes=['text'])
 
 # Main program loop
 if __name__ == '__main__':
     try:
-        # Create an object for working with Telegram
-        updater = ext.Updater(token=TELEGRAM_TOKEN, use_context=True)
-
-        # Create command handlers
-        start_handler = ext.CommandHandler('start', start)
-
-        # Register command handlers
-        updater.dispatcher.add_handler(start_handler)
-
-        # Create message handler
-        message_handler = ext.MessageHandler(
-            ext.Filters.text, handle_message)
-
-        # Register message handler
-        updater.dispatcher.add_handler(message_handler)
-
-        # Start the bot
-        updater.start_polling()
         logging.info("Telegram bot is running and ready to work")
-
-        # Keep the main thread running
-        updater.idle()
-
+        
+        # Start the bot
+        from aiogram import executor
+        executor.start_polling(dp, skip_updates=True)
+        
     except Exception as e:
         logging.error(f"An error occurred while running the Telegram bot: {e}")
     finally:
